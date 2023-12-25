30a15817e01ef5381ba42d7f4b5177fd808168ee
hoc-anms <phamvanhoc96@gmail.com>
2023-12-25 21:45:20 +0700

    feat: update model product

--- src/models/product.model.js
+++ src/models/product.model.js
@@ -1,28 +1,80 @@
-const mongoose = require('mongoose'); // Erase if already required
+'use strict'
+
+const {model, Schema} = require('mongoose'); // Erase if already required
+
+const DOCUMENT_NAME = 'Product';
+const COLLECTION_NAME = 'Products';
 
 // Declare the Schema of the Mongo model
-var userSchema = new mongoose.Schema({
-    name:{
-        type:String,
-        required:true,
-        unique:true,
-        index:true,
-    },
-    email:{
-        type:String,
-        required:true,
-        unique:true,
-    },
-    mobile:{
-        type:String,
-        required:true,
-        unique:true,
-    },
-    password:{
+var productSchema = new Schema({
+    product_name:{
         type:String,
         required:true,
     },
+    product_thumb:{
+        type:String,
+        required:true,
+    },
+    product_description: String,
+    product_price:{
+        type: Number,
+        required:true,
+    },
+    product_quantity:{
+        type: Number,
+        required:true,
+    },
+    product_type:{
+        type:String,
+        required:true,
+        enum: ['Electronics', 'Clothing', 'Furniture'],
+    },
+    product_shop: {
+        type: Schema.Types.ObjectId,
+        ref: 'Shop',
+    },
+    product_attributes: {
+        type: Schema.Types.Mixed,
+        required: true,
+    }
+}, {
+    timestamps: true,
+    colection: COLLECTION_NAME,
 });
 
+
+// Define the product type = clothing
+const clothingSchema = new Schema({
+    brand:{
+        type: String,
+        required:true,
+    },
+    size: String,
+    material: String,
+}, {
+    collation: 'Clothing',
+    timestamps: true,
+})
+
+// Define the product type = electronics
+const electronicSchema = new Schema({
+    manufacture:{
+        type: String,
+        required:true,
+    },
+    model: String,
+    color: String,
+}, {
+    collation: 'Electronics',
+    timestamps: true,
+})
+
+
+
+
 //Export the model
-module.exports = mongoose.model('User', userSchema);
+module.exports = {
+   product: model(DOCUMENT_NAME, productSchema),
+   electronic: model('Electronics', electronicSchema),
+   clothing: model('Clothing', clothingSchema),
+}
