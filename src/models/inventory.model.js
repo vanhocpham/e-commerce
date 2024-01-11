579061d6266182376ba95bec46225294e05866d9
hoc-anms <phamvanhoc96@gmail.com>
2024-01-11 21:13:57 +0700

    feat: update model inventory

---
+++ src/models/inventory.model.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const {model, Schema} = require('mongoose'); // Erase if already required
+
+const DOCUMENT_NAME = 'Inventory';
+const COLLECTION_NAME = 'Inventories';
+
+
+// Declare the Schema of the Mongo model
+var inventorySchema = new Schema({
+    inven_productId: {type: Schema.Types.ObjectId, ref: "Product"},
+    inven_location: {type: String, default: 'unknown'},
+    inven_stock: {type: Number, required: true},
+    inven_shopId: {type: Schema.Types.ObjectId, ref: "Shop"},
+    inven_reservations: {type: Array, default: []},
+
+
+}, {
+    timestamps: true,
+    colection: COLLECTION_NAME,
+});
+
+//Export the model
+module.exports = model(DOCUMENT_NAME, inventorySchema);
