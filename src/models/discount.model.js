74254c01da2d06eccc7e5b44d4a317b7c6f7e7e8
hoc-anms <phamvanhoc96@gmail.com>
2024-01-13 20:40:04 +0700

    feat: update model discount

---
+++ src/models/discount.model.js
@@ -0,0 +1,42 @@
+'use strict'
+
+const {model, Schema} = require('mongoose'); // Erase if already required
+
+const DOCUMENT_NAME = 'Discount';
+const COLLECTION_NAME = 'discounts';
+
+
+// Declare the Schema of the Mongo model
+var discountSchema = new Schema({
+    discount_name:{
+        type: String,
+        required: true,
+    },
+    discount_description:{
+        type: String,
+        required: true,
+    },
+    discount_type:{
+        type: String,
+        default: 'fixed_amount', // or percentage
+    },
+    discount_value: {type: Number, required: true}, //10.000
+    discount_code: {type: String, required: true},
+    discount_start_date: {type: Date, required: true},
+    discount_end_date: {type: Date, required: true},
+    discount_max_uses: {type: Number, required: true},
+    discount_uses_count: {type: Number, required: true},
+    discount_users_used: {type: Array, default: []},
+    discount_max_uses_per_user: {type: Number, required: true},
+    discount_min_order_value: {type: Number, required: true},
+    discount_shopId: {type: Schema.Types.ObjectId, ref: 'Shop'},
+    discount_is_active: {type: Boolean, require: true},
+    discount_applies_to: {type: String, require: true, enum: ['all', 'specific']},
+    discount_product_ids: {type: Array, default: []},
+}, {
+    timestamps: true,
+    colection: COLLECTION_NAME,
+});
+
+//Export the model
+module.exports = model(DOCUMENT_NAME, discountSchema);
