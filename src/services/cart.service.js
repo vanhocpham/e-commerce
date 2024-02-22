83a4ea1f385344a14a2a964ccd72b2328edfeff3
vanhocpham <phamvanhoc96@gmail.com>
2024-02-22 17:24:33 +0700

    feat: init cart service

---
+++ src/services/cart.service.js
@@ -0,0 +1,17 @@
+'use strict'
+
+/**
+ * Key features: Cart Services
+ * - Add product to cart (user)
+ * - reduce product quantity by one (user)
+ * - increase product quantity by one (user)
+ * - get cart (user)
+ * - delete all cart (user)
+ * - delete cart items (user)
+ */
+
+class CartService {
+    static async addToCart({userId, product = {}}){
+        // check cart exists
+    }
+}
