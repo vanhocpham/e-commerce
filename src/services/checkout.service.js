5536d4354486844cdb746f6b706f7758ec128e6a
hoc-anms <phamvanhoc96@gmail.com>
2024-03-08 21:08:31 +0700

    feat: update checkout service

---
+++ src/services/checkout.service.js
@@ -0,0 +1,20 @@
+'use strict'
+
+class CheckoutService {
+    /**
+     * {
+     *      cartId,
+     *      userId,
+     *      shop_order_ids: [
+     *          
+     *      ],
+     * }
+     * @param {*} param0 
+     */
+    static async chekoutReview({cartId, userId, shop_order_ids}){
+
+    }
+
+}
+
+module.exports = CheckoutService;
