fa3698c85b67a64a8dd6cc568f912ad26674a9c4
hoc-anms <phamvanhoc96@gmail.com>
2024-03-10 20:48:28 +0700

    feat: update checkout service

--- src/models/repositories/cart.repo.js
+++ src/models/repositories/cart.repo.js
@@ -1,5 +1,13 @@
 'use strict'
 
 const { Types } = require("mongoose")
-const cart = require("../cart.model")
-const { getSelectData, unGetSelectData } = require('../../utils');
+const {cart} = require("../cart.model")
+const { getSelectData, unGetSelectData, convertToObjectIdMongo } = require('../../utils');
+
+const findCartById = async(cartId) => {
+    return await cart.findOne({_id: convertToObjectIdMongo(cartId), cart_state: 'active'}).lean();
+}
+
+module.exports = {
+    findCartById
+}
--- src/services/checkout.service.js
+++ src/services/checkout.service.js
@@ -1,5 +1,8 @@
 'use strict'
 
+const { BadRequestError } = require("../core/error.response");
+const { findCartById } = require("../models/repositories/cart.repo");
+
 class CheckoutService {
     /**
      * {
@@ -13,6 +16,16 @@ class CheckoutService {
      */
     static async chekoutReview({cartId, userId, shop_order_ids}){
         const foundCart = await findCartById(cartId);
+        if(!foundCart) throw new BadRequestError("Cart does not exist");
+
+        const checkout_order = {
+            total_price: 0,
+            feeShip: 0,
+            totalDiscount: 0,
+            totalCheckout: 0,
+        }, shop_order_ids_new = [];
+
+        //
 
     }
 
