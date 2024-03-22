8c599c3b9f12ec3fed37c60dd9ce8a802300ee8f
hoc-anms <phamvanhoc96@gmail.com>
2024-03-22 21:42:34 +0700

    feat: update inventory repo

--- src/models/repositories/inventory.repo.js
+++ src/models/repositories/inventory.repo.js
@@ -1,20 +1,41 @@
-const { Types } = require("mongoose")
-const { inventory } = require("../inventory.model")
-
+const { Types } = require("mongoose");
+const { inventory } = require("../inventory.model");
+const { convertToObjectIdMongo } = require("../../utils");
 
 const insertInventoryRepo = async ({
-    productId, shopId, stock, location = 'unknown'
+  productId,
+  shopId,
+  stock,
+  location = "unknown",
 }) => {
-    return await inventory.create({
-        inven_productId: productId,
-        inven_shopId: shopId,
-        inven_stock: stock,
-        inven_location: location,
-    })
-}
+  return await inventory.create({
+    inven_productId: productId,
+    inven_shopId: shopId,
+    inven_stock: stock,
+    inven_location: location,
+  });
+};
 
+const reservationInventory = async ({ productId, quantity, cartId }) => {
+  const query = {
+      inven_productId: convertToObjectIdMongo(productId),
+      inven_stock: { $gte: quantity },
+    },
+    updateSet = {
+      $inc: { inven_stock: -quantity },
+      $push: {
+        inven_revations: {
+          quantity,
+          cartId,
+          createOn: new Date(),
+        },
+      },
+    };
+
+    return inventory.updateOne(query, updateSet)
+};
 
 module.exports = {
-    insertInventoryRepo,
-
-}
+  insertInventoryRepo,
+  reservationInventory,
+};
